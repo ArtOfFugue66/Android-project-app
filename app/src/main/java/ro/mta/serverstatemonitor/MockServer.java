891dcf483d000d629a6bcc2e644690874ae1c3e8
ArtOfFugue66 <haifisch1919@gmail.com>
2020-03-20 19:12:32 +0200

    Created MockServer TODOs

--- app/src/main/java/ro/mta/serverstatemonitor/MockServer.java
+++ app/src/main/java/ro/mta/serverstatemonitor/MockServer.java
@@ -1,6 +1,9 @@
 package ro.mta.serverstatemonitor;
 
 public class MockServer {
-    // TODO: implement open ports & service pairs
-
+    // TODO: implement open ports & service pairs (LIST or something type)
+    // TODO: make temperature variable change randomly every 3 seconds (INTEGER/int type)
+    // TODO: implement state (online/offline or up/down) (BOOLEAN type)
+    // TODO: implement uptime (DateTime or something type)
+    // TODO: implement DISA STIG compliant/not-compliant variable (BOOLEAN type)
 }
